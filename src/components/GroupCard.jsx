8bf553afc442fbce4a48c07db2038a3dfc10fe41
Alastair Longmuir <alastairlinwood@gmail.com>
2024-03-06 18:03:32 +0100

    rendered GroupCard with groups from db

---
+++ src/components/GroupCard.jsx
@@ -0,0 +1,13 @@
+const GroupCard = ({groupName}) => {
+    console.log(groupName);
+    return (
+  
+        <div className="">
+          <h1>{groupName}</h1>
+        </div>
+      
+    );
+  };
+  
+  export default GroupCard;
+  
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,34 @@
+import GroupCard from "../components/GroupCard";
+import {useEffect, useState} from 'react';
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL
+
 const DashboardPage = () => {
+
+    const [groups, setGroups] = useState(null);
+
+    const storedToken = localStorage.getItem("authToken");
+    
+    useEffect(() => {
+        axios.get(`${API_URL}/api/groups/user/`, {
+            headers: { Authorization: `Bearer ${storedToken}` },
+          })
+          .then(res => setGroups(res.data))
+          .catch(err => console.log(err))
+    },[])
+
+
+
     return (
         <>
-        <h2>Dashboard</h2>
+        <div><h2>OVERVIEW</h2></div>
+
+        <div>
+            <h2>MY GROUPS</h2>
+            { groups && groups.map(group => <GroupCard groupName={group.groupName} key={group._id} />)}
+        </div>
+        
         </>
     )
 }
