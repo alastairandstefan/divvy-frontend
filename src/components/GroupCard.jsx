1a6c2e2d5583354e819905afacfa5e709c3daf44
Alastair Longmuir <alastairlinwood@gmail.com>
2024-03-11 12:09:52 +0100

    new layout for groups on dashboard

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -4,7 +4,7 @@ const GroupCard = ({ groupName, id }) => {
   return (
     <Link
       to={`/group/${id}`}
-      className="card w-96 h-28 bg-slate-300 shadow-xl m-3 flex justify-center items-center"
+      className="card w-24 h-24 bg-slate-300 shadow-xl m-3 flex justify-center items-center"
     >
       <h1>{groupName}</h1>
     </Link>
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,57 +3,60 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import Expense from "../components/Expense";
 import { Link } from "react-router-dom";
-import { getExpensesOfUser, getGroupsOfUser } from "../components/RetrieveFunctions";
+import {
+  getExpensesOfUser,
+  getGroupsOfUser,
+} from "../components/RetrieveFunctions";
 
 const DashboardPage = () => {
-
-  const {groupsData, isLoadingGroups, groupsError} = getGroupsOfUser();
-  const {expensesData, isLoadingExpenses, expensesError} = getExpensesOfUser();
-
+  const { groupsData, isLoadingGroups, groupsError } = getGroupsOfUser();
+  const { expensesData, isLoadingExpenses, expensesError } =
+    getExpensesOfUser();
 
   if (isLoadingGroups || isLoadingExpenses) return <div>Loading...</div>;
   if (groupsError || expensesError) return <div>An error has occurred.</div>;
 
   return (
     <div className="flex flex-col items-center justify-between h-[90%] p-3">
-      <div className="flex flex-col items-center">
-      <h2 className="self-start">OVERVIEW</h2>
-        {expensesData.slice(0,2).map((expense) => (
-          <Expense
-            key={expense._id}
-            amount={expense.amount}
-            expenseName={expense.expenseName}
-            payer={expense.payer}
-            splits={expense.splits}
-            expenseId = {expense._id}
-          />
-        ))}
-
-        {expensesData.length > 2 && (
-          <button className="btn btn-sm rounded-2xl border-1 border-slate-500">
-            Show full list
-          </button>
-        )}
-      </div>
-
-      <div className="flex flex-col items-center">
-        <h2 className="self-start">MY GROUPS</h2>
-        {groupsData &&
-          groupsData.map((group) => (
-            <GroupCard
-              groupName={group.groupName}
-              key={group._id}
-              id={group._id}
+      <div>
+        <h2>OVERVIEW</h2>
+        <div className="flex flex-col items-center">
+          {expensesData.slice(0, 2).map((expense) => (
+            <Expense
+              key={expense._id}
+              amount={expense.amount}
+              expenseName={expense.expenseName}
+              payer={expense.payer}
+              splits={expense.splits}
+              expenseId={expense._id}
             />
           ))}
-        <button className="btn w-96 border-1 border-slate-500 border-dashed bg-transparent mt-3">
-          +
-        </button>
+
+          {expensesData.length > 2 && (
+            <button className="btn btn-sm rounded-2xl border-1 border-slate-500">
+              Show full list
+            </button>
+          )}
+        </div>
       </div>
-      <div className="flex justify-center">
-        <Link to={`/expense/create`} className="btn btn-md rounded-3xl border-1 border-slate-500">
-          Add Expense
-        </Link>
+
+      <div>
+        <h2>MY GROUPS</h2>
+        <div className="flex justify-center">
+          <div className="flex flex-wrap">
+            {groupsData &&
+              groupsData.map((group) => (
+                <GroupCard
+                  groupName={group.groupName}
+                  key={group._id}
+                  id={group._id}
+                />
+              ))}
+            <button className="btn w-24 h-24 border-1 border-slate-500 border-dashed bg-transparent m-3">
+              +
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
