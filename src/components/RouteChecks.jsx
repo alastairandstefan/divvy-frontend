03472edb7465680637f4731e88ec75ae2bdb44ce
Alastair Longmuir <alastairlinwood@gmail.com>
2024-03-11 15:26:41 +0100

    fixed private/anon routing

--- src/components/RouteChecks.jsx
+++ src/components/RouteChecks.jsx
@@ -2,43 +2,34 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/auth.context";
 import { useNavigate } from "react-router-dom";
 
+export const PrivateRoute = ({ children }) => {
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export const PrivateRoute = ({children}) => {
+  // If the authentication is still loading
+  if (isLoading) return <p>Loading ...</p>;
 
-    const { isLoggedIn, isLoading } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    // If the authentication is still loading 
-    if (isLoading) return <p>Loading ...</p>;
-  
-    if (!isLoggedIn) {
+  if (!isLoggedIn) {
     // If the user is not logged in (redirect to landing page)
-        useEffect(() => {
-            navigate(`/`)
-        },[])
-    } else {
-    // If the user is logged in, allow to see the page 
-      return children;
-    }
+    navigate(`/`);
+  } else {
+    // If the user is logged in, allow to see the page
+    return children;
+  }
+};
 
-}
+export const AnonymousRoute = ({ children }) => {
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-export const AnonymousRoute = ({children}) => {
+  // If the authentication is still loading
+  if (isLoading) return <p>Loading ...</p>;
 
-    const { isLoggedIn, isLoading } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    // If the authentication is still loading 
-    if (isLoading) return <p>Loading ...</p>;
-  
-    if (isLoggedIn) {
-    // If the user is logged in 
-    useEffect(() => {
-        navigate(`/dashboard`)
-    },[])
-    } else {
+  if (isLoggedIn) {
+    // If the user is logged in
+    navigate(`/dashboard`);
+  } else {
     // If the user is not logged in, allow to see the page (signup/login)
-      return children;
-    }
-
-}
+    return children;
+  }
+};
